e24329cd1fb3e244004fb7f792c67783b04170e1
Denis <109104246+Denis0014@users.noreply.github.com>
2023-02-26 20:53:16 +0300

    Задание 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,58 @@ namespace HomeWork2
 {
     public class Program
     {
+        /// <summary>
+        /// Печатает массив на экран
+        /// </summary>
+        /// <param name="arr">Исходный массив</param>
+        public static void PrintArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]} ");
+        }
+        /// <summary>
+        /// Печатает матрицу на экран
+        /// </summary>
+        /// <param name="matr">Исходная матрица</param>
+        public static void PrintMatr(int[,] matr)
+        {
+            for (int i = 0; i < matr.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr.GetLength(1); j++)
+                    Console.Write($"{matr[i, j]} ");
+                Console.WriteLine();
+            }
+        }
+        /// <summary>
+        /// Возвращает новый массив, в котором удалены все элементы, не удовлетворяющие предикату
+        /// </summary>
+        /// <param name="arr">Исходный массив</param>
+        /// <param name="f">Булевый предикат</param>
+        /// <returns>Массив с такимже или меньшим количество элементов</returns>
+        public static int[] Where(int[] arr, Func<int, bool> f)
+        {
+            int[] result = new int[arr.Length];
+            int c = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (f(arr[i]))
+                    result[c++] = arr[i];
+            Array.Resize(ref result, c);
+            return result;
+        }
+        /// <summary>
+        /// Возвращает нечётное число или нет
+        /// </summary>
+        /// <param name="num">Исходное число</param>
+        /// <returns>Число нечётное?</returns>
+        public static bool Odd(int num) => num % 2 != 0;
+        /// <summary>
+        /// Возвращает чётное число или нет
+        /// </summary>
+        /// <param name="num">Исходное число</param>
+        /// <returns>Число чётное?</returns>
+        public static bool Even(int num) => num % 2 == 0;
         static void Main(string[] args)
         {
-            
         }
     }
 }
---
+++ TestHomeWork2/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using static HomeWork2.Program;
+
+namespace TestHomeWork2
+{
+    public class Tests
+    {
+        [Test]
+        public void WhereTest()
+        {
+            Assert.That(Where(new int[] { 1, 2, 3, 4, 5 }, Odd), Is.EqualTo(new int[] { 1, 3, 5 }));
+            Assert.That(Where(new int[] { 1, 2, 3, 4, 5 }, Even), Is.EqualTo(new int[] { 2, 4 }));
+        }
+    }
+}
